0d87ff702f2d4bd9dd206d1d5ed99a9ac6887ef5
Megalodon561 <vano050878@gmail.com>
2022-08-10 16:25:58 +0300

    lesson 2

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.SetWindowSize(30, 40);
+Console.SetBufferSize(30, 40);
+static void Drow(int x, int y, char c)
+{
+    Console.SetCursorPosition(x, y);
+    Console.Write(c);
+    Console.ReadLine();
+}
