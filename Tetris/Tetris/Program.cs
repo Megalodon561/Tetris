de612545e77ccdc99fa46fbe308ac6d01d074ae9
Megalodon561 <vano050878@gmail.com>
2022-08-10 17:08:11 +0300

    добавил класс Точка

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -1,8 +1,2 @@
 ﻿Console.SetWindowSize(30, 40);
 Console.SetBufferSize(30, 40);
-static void Draw(int x, int y, char c)
-{
-    Console.SetCursorPosition(x, y);
-    Console.Write(c);
-    Console.ReadLine();
-}
