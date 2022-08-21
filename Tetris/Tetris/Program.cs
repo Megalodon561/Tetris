4dbb6f802b6ae5372a7b6b29055b426643c0797a
Megalodon561 <vano050878@gmail.com>
2022-08-21 14:36:59 +0300

    добавил класс квадрат

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -1,3 +1,31 @@
 ﻿using Tetris;
 Console.SetWindowSize(30, 40);
 Console.SetBufferSize(30, 40);
+Squares s = new Squares(0, 0, '*');
+s.Draw();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Console.ReadLine();
